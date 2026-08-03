e2bdbcdb1b0babd3a90f060a204e77e671f6c6a2
Roland Cozzolino <rgc.syntin@gmail.com>
2026-08-03 09:07:05 -0400

    Removing old layout

--- OlympusMons/Olympus/Source/Common/Time.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-namespace Time
-{
-	std::wstring OLYMPUS_API GetTime(BOOL stripped = FALSE);
-	std::wstring OLYMPUS_API GetDate(BOOL stripped = FALSE);
-	std::wstring OLYMPUS_API GetDateTimeString(BOOL stripped = FALSE);
-}
